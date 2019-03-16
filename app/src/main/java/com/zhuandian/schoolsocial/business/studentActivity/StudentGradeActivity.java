0a94f291a538567de3521ca8238b040cf8374ce8
xiedong <xiedong11@aliyun.com>
2019-03-16 10:23:18 +0800

    修改成绩查询页面

--- app/src/main/java/com/zhuandian/schoolsocial/business/studentActivity/StudentGradeActivity.java
+++ app/src/main/java/com/zhuandian/schoolsocial/business/studentActivity/StudentGradeActivity.java
@@ -1,18 +1,16 @@
 package com.zhuandian.schoolsocial.business.studentActivity;
 
-import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.zhuandian.schoolsocial.R;
 import com.zhuandian.schoolsocial.base.BaseActivity;
-import com.zhuandian.schoolsocial.entity.ScoreEntity;
-import com.zhuandian.schoolsocial.entity.UserEntity;
+import com.zhuandian.schoolsocial.entity.StudentScoreEntity;
+
 
 import java.util.List;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.exception.BmobException;
@@ -35,22 +33,23 @@ public class StudentGradeActivity extends BaseActivity {
 
     @Override
     protected void setUpView() {
+        BmobQuery<StudentScoreEntity> query = new BmobQuery<>();
+        query.findObjects(new FindListener<StudentScoreEntity>() {
+            @Override
+            public void done(List<StudentScoreEntity> list, BmobException e) {
+                if (e == null) {
+                    StudentScoreEntity studentScoreEntity = list.get(0);
+                    tvScore.setText(String.format("成绩分布：\n\n60分以下：%s人 \n60-80人数： %s人 \n80-90分： %s人\n90分以上：%s人",
+                            studentScoreEntity.getScore60(), studentScoreEntity.getScore60_80(), studentScoreEntity.getScore80_90(), studentScoreEntity.getScore90()
+                    ));
+                }
+            }
+        });
     }
 
 
     @OnClick(R.id.tv_submit)
     public void onClick() {
-        BmobQuery<ScoreEntity> bmobQuery = new BmobQuery<>();
-        bmobQuery.addWhereEqualTo("userName", etUsername.getText().toString())
-                .findObjects(new FindListener<ScoreEntity>() {
-                    @Override
-                    public void done(List<ScoreEntity> list, BmobException e) {
-                        if (e == null) {
-                            tvScore.setText(list.get(0).getScores());
-                        } else {
-                            tvScore.setText("暂时未查到该学生的成绩信息...");
-                        }
-                    }
-                });
+
     }
 }
---
+++ app/src/main/java/com/zhuandian/schoolsocial/entity/StudentScoreEntity.java
@@ -0,0 +1,47 @@
+package com.zhuandian.schoolsocial.entity;
+
+import java.util.List;
+
+/**
+ * desc :
+ * author：xiedong
+ * date：2019/3/14
+ */
+public class StudentScoreEntity {
+    private String score60;
+    private String score60_80;
+    private String score80_90;
+    private String score90;
+
+    public String getScore60() {
+        return score60;
+    }
+
+    public void setScore60(String score60) {
+        this.score60 = score60;
+    }
+
+    public String getScore60_80() {
+        return score60_80;
+    }
+
+    public void setScore60_80(String score60_80) {
+        this.score60_80 = score60_80;
+    }
+
+    public String getScore80_90() {
+        return score80_90;
+    }
+
+    public void setScore80_90(String score80_90) {
+        this.score80_90 = score80_90;
+    }
+
+    public String getScore90() {
+        return score90;
+    }
+
+    public void setScore90(String score90) {
+        this.score90 = score90;
+    }
+}
